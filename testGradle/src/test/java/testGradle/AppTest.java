946e374c8e79d343096fc881a7ebf6fd1d2e7bac
hystepik <lucas.marcouiller@etu.u-bordeaux.fr>
2021-01-11 10:50:41 +0100

    test

---
+++ testGradle/src/test/java/testGradle/AppTest.java
@@ -0,0 +1,17 @@
+package testGradle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testCovidChanceEnMoyenne() {
+	}
+
+	@Test
+	public void testCovidChanceDuree() {
+	}
+
+}
